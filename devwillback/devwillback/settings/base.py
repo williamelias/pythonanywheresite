34698b91577cb0befc5a2328af71ead9d993fa93
williamelias <williameliasalves@gmail.com>
2025-02-16 22:14:23 -0300

    refactor: ignore some itens.

--- devwillback/devwillback/settings/base.py
+++ devwillback/devwillback/settings/base.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 if os.getenv("ENVIRONMENT", default="develop") == "production":
